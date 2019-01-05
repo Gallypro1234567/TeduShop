d64aca7e900fb3f32913f8414c84437fdee6b347
Gallypro1234567 <tranductrong140198@gmail.com>
2019-01-05 23:18:16 +0700

    test $scope vsd $rootscope

---
+++ TeduShop.Wed/Scripts/spa/app.js
@@ -0,0 +1,19 @@
+﻿/// <reference path="../plugins/angular/angular.js" />
+
+var myApp = angular.module('myModule', []);
+
+myApp.controller("StudentController", StudentController);
+myApp.controller("TeacherController", TeacherController);
+myApp.controller("SchoolController", SchoolController);
+ 
+
+// khoi tao controll
+function StudentController($scope) {
+    $scope.message = "this is my love when i was student";
+}
+function TeacherController($scope) {
+    $scope.message = " this is my teacher in life";
+}
+function SchoolController($scope) {
+    $scope.message= "this is my school THPT Thuanhoa"
+}

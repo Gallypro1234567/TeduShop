0d49fa9bdb29e53f8b4f821f35b9e329bdb24756
TRỌNG TRẦN <tranductrong140198@gmail.com>
2019-02-17 00:50:00 +0700

    update form and seo title service

--- TeduShop.Data/Migrations/Configuration.cs
+++ TeduShop.Data/Migrations/Configuration.cs
@@ -20,31 +20,31 @@
         protected override void Seed(TeduShop.Data.TedushopDbContext context)
         {
             CreatProductCategorySample(context);
-           /* //  This method will be called after migrating to the latest version.
-            var manager = new UserManager<ApplicationUser>(new UserStore<ApplicationUser>(new TedushopDbContext()));
-            var roleManager = new RoleManager<IdentityRole>(new RoleStore<IdentityRole>(new TedushopDbContext()));
-            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
-            //  to avoid creating duplicate seed data.
-            var user = new ApplicationUser()
-            {
-                UserName = "tedu",
-                Email = "Tefu.international@gmail.com",
-                EmailConfirmed = true,
-                BirthDay = DateTime.Now,              
-                FullName = "Technology Education"
-            };
-            manager.Create(user, "123654$");
-            if (!roleManager.Roles.Any())
-            {
-                roleManager.Create(new IdentityRole { Name = "Admin" });
-                roleManager.Create(new IdentityRole { Name = "User" });
-            }
-            var adminUser = manager.FindByEmail("Tefu.international@gmail.com");
-            manager.AddToRoles(adminUser.Id, new string[] { "admin", "User" });*/
+             //  This method will be called after migrating to the latest version.
+             var manager = new UserManager<ApplicationUser>(new UserStore<ApplicationUser>(new TedushopDbContext()));
+             var roleManager = new RoleManager<IdentityRole>(new RoleStore<IdentityRole>(new TedushopDbContext()));
+             //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
+             //  to avoid creating duplicate seed data.
+             var user = new ApplicationUser()
+             {
+                 UserName = "tedu",
+                 Email = "Tefu.international@gmail.com",
+                 EmailConfirmed = true,
+                 BirthDay = DateTime.Now,              
+                 FullName = "Technology Education"
+             };
+             manager.Create(user, "123654$");
+             if (!roleManager.Roles.Any())
+             {
+                 roleManager.Create(new IdentityRole { Name = "Admin" });
+                 roleManager.Create(new IdentityRole { Name = "User" });
+             }
+             var adminUser = manager.FindByEmail("Tefu.international@gmail.com");
+             manager.AddToRoles(adminUser.Id, new string[] { "admin", "User" });
         }
         private void CreatProductCategorySample(TeduShop.Data.TedushopDbContext context)
         {
-            if(context.ProductCategories.Count()==0)
+            if (context.ProductCategories.Count() == 0)
             {
                 List<ProductCategory> listProductCategory = new List<ProductCategory>()
             {
@@ -56,7 +56,6 @@
                 context.ProductCategories.AddRange(listProductCategory);
                 context.SaveChanges();
             }
-         }
+        }
     }
 }
- 
--- TeduShop.Wed/API/ProductCategoryController.cs
+++ TeduShop.Wed/API/ProductCategoryController.cs
@@ -17,13 +17,17 @@ namespace TeduShop.Wed.Api
     [RoutePrefix("api/productcategory")]
     public class ProductCategoryController : ApiControllerBase
     {
-        IProductCategoryService _productCategoryService;
-
+        #region Intitialize
+        private IProductCategoryService _productCategoryService;
         public ProductCategoryController(IErrorService errorService, IProductCategoryService productCategoryService)
-            : base(errorService)
+           : base(errorService)
         {
             this._productCategoryService = productCategoryService;
         }
+        #endregion
+        
+
+       
         [Route("getallparents")]
         [HttpGet]
         public HttpResponseMessage GetAll(HttpRequestMessage request)
@@ -35,6 +39,21 @@ namespace TeduShop.Wed.Api
                 var responseData = Mapper.Map<IEnumerable<ProductCategory>, IEnumerable<ProductCategoryViewModel>>(model);
 
                 var response = request.CreateResponse(HttpStatusCode.OK, responseData);
+                return response;
+            });
+        }
+        [Route("getbyid/{id:int}")]
+        [HttpGet]
+        public HttpResponseMessage GetById(HttpRequestMessage request, int id)
+        {
+            return CreateHttpResponse(request, () =>
+            {
+                var model = _productCategoryService.GetById(id);
+
+                var responseData = Mapper.Map<ProductCategory, ProductCategoryViewModel>(model);
+
+                var response = request.CreateResponse(HttpStatusCode.OK, responseData);
+
                 return response;
             });
         }
@@ -79,7 +98,7 @@ namespace TeduShop.Wed.Api
                 {
                     var newProductCategory = new ProductCategory();
                     newProductCategory.UpdateProductCategory(productCategoryVm);
-
+                    newProductCategory.CreatedDate = DateTime.Now;
                     _productCategoryService.Add(newProductCategory);
                     _productCategoryService.Save();
 
@@ -90,5 +109,38 @@ namespace TeduShop.Wed.Api
                 return response;
             });
         }
+
+         [Route("update")]
+        [HttpPut]
+        [AllowAnonymous]
+        public HttpResponseMessage Update(HttpRequestMessage request, ProductCategoryViewModel productCategoryVm)
+        {
+            return CreateHttpResponse(request, () =>
+            {
+                HttpResponseMessage response = null;
+                if (!ModelState.IsValid)
+                {
+                    response = request.CreateResponse(HttpStatusCode.BadRequest, ModelState);
+                }
+                else
+                {
+                    var dbProductCategory = _productCategoryService.GetById(productCategoryVm.ID);
+
+                    dbProductCategory.UpdateProductCategory(productCategoryVm);
+                    dbProductCategory.UpdateDate = DateTime.Now;             
+
+                    _productCategoryService.Update(dbProductCategory);
+                    _productCategoryService.Save();
+
+                    var responseData = Mapper.Map<ProductCategory, ProductCategoryViewModel>(dbProductCategory);
+                    response = request.CreateResponse(HttpStatusCode.Created, responseData);
+                }
+
+                return response;
+            });
+        }
+
     }
+
 }
+
